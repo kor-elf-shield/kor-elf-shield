a0d3f0235e82e6850c8c3a6bd80986c17e2f5f2f
Leonid Nikitin <i@kor-elf.net>
2025-10-07 22:39:17 +0500

    Improve i18n initialization and dynamic locale loading; handle errors in language changes.

--- internal/i18n/i18n.go
+++ internal/i18n/i18n.go
@@ -3,6 +3,7 @@ package i18n
 import (
 	"embed"
 	"encoding/json"
+	"fmt"
 
 	"github.com/nicksnyder/go-i18n/v2/i18n"
 	"golang.org/x/text/language"
@@ -17,10 +18,12 @@ var (
 
 type langContract interface {
 	T(messageID string, data ...map[string]interface{}) string
-	ChangeLang(lang string)
+	ChangeLang(lang string) error
 }
 
 type lang struct {
+	loadedLangs map[string]bool
+
 	bundler           *i18n.Bundle
 	localizer         *i18n.Localizer
 	fallbackLocalizer *i18n.Localizer
@@ -29,22 +32,29 @@ type lang struct {
 	currentLang  string
 }
 
-func InitLang(fallbackLang string) {
+func InitLang(fallbackLang string) error {
 	bundle := i18n.NewBundle(language.Make(fallbackLang))
 	bundle.RegisterUnmarshalFunc("json", json.Unmarshal)
-	_, _ = bundle.LoadMessageFileFS(localeFiles, "locales/locale.ru.json")
-	_, _ = bundle.LoadMessageFileFS(localeFiles, "locales/locale.en.json")
 
-	localizer := i18n.NewLocalizer(bundle, fallbackLang)
-
-	Lang = &lang{
-		bundler:           bundle,
-		localizer:         localizer,
-		fallbackLocalizer: localizer,
+	newLang := lang{
+		loadedLangs: make(map[string]bool),
+		bundler:     bundle,
 
 		fallbackLang: fallbackLang,
 		currentLang:  fallbackLang,
 	}
+
+	err := newLang.ensureLocaleLoaded(fallbackLang)
+	if err != nil {
+		return err
+	}
+
+	localizer := i18n.NewLocalizer(bundle, fallbackLang)
+	newLang.localizer = localizer
+	newLang.fallbackLocalizer = localizer
+
+	Lang = &newLang
+	return nil
 }
 
 func (l *lang) T(messageID string, data ...map[string]interface{}) string {
@@ -62,13 +72,19 @@ func (l *lang) T(messageID string, data ...map[string]interface{}) string {
 	return msg
 }
 
-func (l *lang) ChangeLang(lang string) {
+func (l *lang) ChangeLang(lang string) error {
 	if lang == l.currentLang {
-		return
+		return nil
+	}
+
+	err := l.ensureLocaleLoaded(lang)
+	if err != nil {
+		return err
 	}
 
 	l.localizer = i18n.NewLocalizer(l.bundler, lang)
 	l.currentLang = lang
+	return nil
 }
 
 func (l *lang) fallbackT(messageID string, data ...map[string]interface{}) string {
@@ -84,4 +100,20 @@ func (l *lang) fallbackT(messageID string, data ...map[string]interface{}) strin
 		return messageID
 	}
 	return msg
+}
+
+// We upload translation files as needed.
+func (l *lang) ensureLocaleLoaded(lang string) error {
+	if l.loadedLangs[lang] {
+		return nil
+	}
+
+	path := fmt.Sprintf("locales/locale.%s.json", lang)
+	_, err := l.bundler.LoadMessageFileFS(localeFiles, path)
+	if err != nil {
+		return err
+	}
+
+	l.loadedLangs[lang] = true
+	return nil
 }
