a7b88ff4feabf03850da02533cd5e21a69a308a9
Leonid Nikitin <i@kor-elf.net>
2025-10-13 20:41:30 +0500

    Add `pid_file` configuration and validation in `setting` package
    - Introduced `pid_file` field in configuration with default path.
    - Implemented validation for `pid_file` in `ToDaemonOptions` with localized error handling.
    - Updated `kor-elf-shield.toml` to include `pid_file` documentation.

--- internal/setting/setting.go
+++ internal/setting/setting.go
@@ -1,8 +1,15 @@
 package setting
 
+import (
+	"errors"
+	"kor-elf-shield/internal/daemon"
+	"kor-elf-shield/internal/i18n"
+)
+
 type setting struct {
 	Language         string `mapstructure:"language"`
 	FallbackLanguage string `mapstructure:"fallback_language"`
+	PidFile          string `mapstructure:"pid_file"`
 
 	Log *log
 }
@@ -11,7 +18,20 @@ func settingDefault() *setting {
 	return &setting{
 		Language:         "ru",
 		FallbackLanguage: "ru",
+		PidFile:          "/var/run/kor-elf-shield/kor-elf-shield.pid",
 
 		Log: logDefault(),
 	}
+}
+
+func (s setting) ToDaemonOptions() (daemon.DaemonOptions, error) {
+	if s.PidFile == "" {
+		return daemon.DaemonOptions{}, errors.New(i18n.Lang.T("parameter is not specified", map[string]any{
+			"Parameter": "pid_file",
+		}))
+	}
+
+	return daemon.DaemonOptions{
+		PathPidFile: s.PidFile,
+	}, nil
 }
