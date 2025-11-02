f81085dec2cb6d59f052297035601c9322c0a8a2
Leonid Nikitin <i@kor-elf.net>
2025-11-02 14:52:22 +0500

    Reorganize imports in `main.go` for better readability

--- internal/cmd/main.go
+++ internal/cmd/main.go
@@ -3,12 +3,13 @@ package cmd
 import (
 	"context"
 	"fmt"
-	"git.kor-elf.net/kor-elf-shield/kor-elf-shield/internal/cmd/daemon"
-	"git.kor-elf.net/kor-elf-shield/kor-elf-shield/internal/i18n"
 	"os"
 	"os/signal"
 	"strings"
 	"syscall"
+
+	"git.kor-elf.net/kor-elf-shield/kor-elf-shield/internal/cmd/daemon"
+	"git.kor-elf.net/kor-elf-shield/kor-elf-shield/internal/i18n"
 
 	"github.com/urfave/cli/v3"
 )
