c0573c4e366956b7206de37d1fb4a59b42d6151c
Leonid Nikitin <i@kor-elf.net>
2026-03-19 19:48:37 +0500

    Add `Config` struct for blocklist configuration management
    - Introduced `Config` and `SourceConfig` structs to manage blocklist repository and source settings.

---
+++ internal/daemon/blocklist/config.go
@@ -0,0 +1,19 @@
+package blocklist
+
+import (
+	"time"
+
+	"git.kor-elf.net/kor-elf-shield/kor-elf-shield/internal/daemon/blocklist/sources"
+	"git.kor-elf.net/kor-elf-shield/kor-elf-shield/internal/daemon/db/repository"
+)
+
+type Config struct {
+	BlocklistRepository repository.BlocklistRepository
+	Sources             []*SourceConfig
+}
+
+type SourceConfig struct {
+	Name     string
+	Interval time.Duration
+	Source   sources.BlocklistSource
+}
