6438358a53b549b15c80d8c71e5829826fdf903b
Leonid Nikitin <i@kor-elf.net>
2026-03-19 19:45:38 +0500

    Add `ReplaceElements` method for batch updating firewall blocklist
    - Introduced `ReplaceElements` to allow bulk replacement of blocklist entries.
    - Implemented `chunkStrings` helper for batching updates efficiently.

--- internal/daemon/firewall/chain/block/list.go
+++ internal/daemon/firewall/chain/block/list.go
@@ -2,6 +2,7 @@ package block
 
 import (
 	"fmt"
+	"strings"
 
 	nft "git.kor-elf.net/kor-elf-shield/go-nftables-client"
 	"git.kor-elf.net/kor-elf-shield/go-nftables-client/family"
@@ -11,6 +12,7 @@ type List interface {
 	Name() string
 	AddElement(element string) error
 	DeleteElement(element string) error
+	ReplaceElements(elements []string) error
 }
 
 type list struct {
@@ -56,4 +58,44 @@ func (l *list) DeleteElement(element string) error {
 		fmt.Sprintf("{ %s }", element),
 	}
 	return l.nft.Command().Run(command...)
+}
+
+func (l *list) ReplaceElements(elements []string) error {
+	if len(elements) == 0 {
+		return nil
+	}
+
+	if err := l.nft.Command().Run("flush set", l.family.String(), l.table, l.name); err != nil {
+		return err
+	}
+
+	const batchSize = 200
+	for _, batch := range chunkStrings(elements, batchSize) {
+		command := []string{
+			"add element",
+			l.family.String(), l.table, l.name,
+			fmt.Sprintf("{ %s }", strings.Join(batch, ",")),
+		}
+		if err := l.nft.Command().Run(command...); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func chunkStrings(items []string, size int) [][]string {
+	if size <= 0 {
+		size = 100
+	}
+
+	chunks := make([][]string, 0, (len(items)+size-1)/size)
+	for start := 0; start < len(items); start += size {
+		end := start + size
+		if end > len(items) {
+			end = len(items)
+		}
+		chunks = append(chunks, items[start:end])
+	}
+	return chunks
 }
