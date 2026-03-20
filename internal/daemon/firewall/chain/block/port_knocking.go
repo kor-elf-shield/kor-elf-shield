2a2ec666e67f9d90e1eddcde1e701ab5e24b7539
Leonid Nikitin <i@kor-elf.net>
2026-03-20 21:22:08 +0500

    Add `NewPortKnocking` method to `block` for port knocking rule initialization

---
+++ internal/daemon/firewall/chain/block/port_knocking.go
@@ -0,0 +1,20 @@
+package block
+
+import (
+	"strconv"
+	"strings"
+
+	nft "git.kor-elf.net/kor-elf-shield/go-nftables-client"
+	"git.kor-elf.net/kor-elf-shield/go-nftables-client/family"
+	"git.kor-elf.net/kor-elf-shield/kor-elf-shield/internal/pkg/ip"
+)
+
+func NewPortKnocking(nft nft.NFT, family family.Type, table string, name string, ipVersion ip.Version, timeout uint32) error {
+	params := []string{"type", ipVersion.ToNftForSet() + ";", "flags timeout; timeout", strconv.Itoa(int(timeout)) + "s;"}
+	_, err := newList(nft, family, table, name, strings.Join(params, " "))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
