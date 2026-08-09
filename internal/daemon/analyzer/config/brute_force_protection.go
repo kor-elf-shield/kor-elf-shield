aa0c5a95511078c62a0d2214c0a6cb40e7782435
Leonid Nikitin <i@kor-elf.net>
2026-08-09 20:14:50 +0500

    Update: enhance regex pattern to include `publickey` authentication failures in brute force protection logs

--- internal/daemon/analyzer/config/brute_force_protection.go
+++ internal/daemon/analyzer/config/brute_force_protection.go
@@ -29,14 +29,14 @@ func NewBruteForceProtectionSSH(isNotify bool, notifyCooldown int, notifyEvery i
 
 			Patterns: []brute_force_protection.RegexPattern{
 				{
-					Regexp: regular_expression.NewLazyRegexp(`^Failed password for (invalid user |illegal user )?(\S*) from (\S+)( port \S+ \S+\s*)`),
+					Regexp: regular_expression.NewLazyRegexp(`^Failed (password|publickey) for (invalid user |illegal user )?(\S*) from (\S+)( port \S+ \S+\s*)`),
 					Values: []brute_force_protection.PatternValue{
 						{
 							Name:  i18n.Lang.T("user"),
-							Value: 2,
+							Value: 3,
 						},
 					},
-					IP: 3,
+					IP: 4,
 				},
 			},
 			Group: group,
